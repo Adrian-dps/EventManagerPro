d41a3c80af76a29c620d67cde4d482557fea6af7
alffern <alfredodocil@gmail.com>
2017-12-27 19:23:12 +0100

    1 prueba

---
+++ app/src/main/java/moviles/uva/eventmanagerpro/MainActivity.java
@@ -0,0 +1,13 @@
+package moviles.uva.eventmanagerpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
